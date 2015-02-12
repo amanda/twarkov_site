bf4c8681c74a4be3b8271e431ea733b3458c2ab6
amandapickering <pickering.amanda@gmail.com>
2015-02-12 13:12:46 -0500

    no debug for heroku (again)

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ def generate_tweet():
 
 # run the app
 if __name__ == '__main__':
-    app.run(debug=True)
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
